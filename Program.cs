d98e194d05f5481ca2a2022c7c7f3d4ef4c864c4
MauroVA <162586492+Mauyo64@users.noreply.github.com>
2026-06-05 11:48:24 +0200

    Startscherm verder uitgewerkt + kleur toegepast

--- Program.cs
+++ Program.cs
@@ -8,30 +8,42 @@ public class Program
     public static void Main(string[] args)
     {
         List<string> ticket = new();
-        Console.WriteLine("======================================");
+        Console.WriteLine("==========================================");
         Console.WriteLine("            WARENHUIS OVERFLOW");
         Console.WriteLine("         Stapelplein 1, 9000 Gent");
-        Console.WriteLine("         Tel: 09 234 56 78");
-        Console.WriteLine("======================================");
-        Console.WriteLine($"Ticket:{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
-        Console.WriteLine($"Datum: {DateTime.Now:yyyy-MM-dd HH:mm}");
-        Console.WriteLine("--------------------------------------");
+        Console.WriteLine("            Tel: 09 234 56 78");
+        Console.WriteLine("           BTW: BE 0123.456.789");
+        Console.WriteLine("==========================================");
+        Console.WriteLine($"  Ticket: {DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}");
+        Console.WriteLine($"  Datum:  {DateTime.Now:yyyy-MM-dd HH:mm}");
+        Console.WriteLine("------------------------------------------");
 
         if (ticket.Count == 0)
         {
-            Console.WriteLine("(Leeg)");
+            Console.WriteLine("  (leeg)");
         }
         else
-        { foreach (var item in ticket)
-            { 
-                Console.WriteLine(item); 
+        {
+            foreach (var item in ticket)
+            {
+                Console.WriteLine(item);
             }
         }
 
-        Console.WriteLine("======================================");
+        Console.WriteLine("==========================================");
+        Console.WriteLine("");
 
+        Console.ForegroundColor = ConsoleColor.DarkGray;
 
+        Console.WriteLine(" <scan barcode> of [barcode]<Enter> | [aantal extra]<Enter>");
+        Console.WriteLine(" [D]<Enter> = verwijderen | [Z]<Enter> = undo-laatste");
+        Console.WriteLine(" [K]<Enter> = betalen met Kaart | [C]<Enter> = betaald met Cash");
+        Console.WriteLine(" [P]<Enter> = parkeren | [H]<Enter> = hervatten | [A]<Enter> = afbreken");
 
+        Console.ForegroundColor = ConsoleColor.White;
+
+        Console.WriteLine();
+        Console.Write("> ");
         // ... 
     }
     // ... 
